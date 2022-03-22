e1df7a56d34ae5834b196818bfc996e1111fc51f
Minjun Choi <mmm830@g.skku.edu>
2022-03-22 16:16:23 +0900

    Semi-Final Result

--- Assignment1_2/2021314533_Calc.cpp
+++ Assignment1_2/2021314533_Calc.cpp
@@ -1,23 +1,11 @@
-﻿#include <iostream>
+﻿//Student Profile : 2021314533 최민준
+//Algorithm Assignment#1
+#include <iostream>
 #include <cstdlib>
 #include <stack>
 #include <vector>
 #include <cmath>
-
-//operator precedence will be stored using this class
-//class op_precedence
-//{
-//public:
-//    op_precedence(char name, int prec)
-//        :op_name(name), precedence(prec)
-//    {
-//
-//    }
-//private:
-//    char op_name;
-//    int precedence;
-//};
-
+//this class stores distinct operand/operator from given input
 class op
 {
 public:
@@ -31,10 +19,12 @@ public:
     void setValue(float temp_f) { _value = temp_f; }
     void setIsOperand(bool temp_b) { _IsOperand = temp_b; }
 private:
-    bool _IsOperand;
-    float _value;
+    bool _IsOperand; // true if it is operand
+    float _value; // integer value(ASCII value converted to float if operator)
 };
 //functions
+//This function returns the index of certain operator 
+//designated with enum OP_index in main function
 int CharToIndex(char Operator)
 {
     switch (Operator)
@@ -54,6 +44,7 @@ int CharToIndex(char Operator)
     }
     return -1;
 }
+//Simple calculation with two float values and an operator
 float SimpleCalc(float left, float right, int Op_Char)
 {
     if (Op_Char == '+') {
@@ -70,39 +61,31 @@ float SimpleCalc(float left, float right, int Op_Char)
     }
     else return -1.0;
 }
-
-float Calc(std::vector<op> postfix_stack)
+//This function performs calculation of postfix notation
+float Calc(std::vector<op> postfix_vector)
 {
-    if (postfix_stack.size() == 0) {
-        return -1.0;
+    if (postfix_vector.size() == 0) { // if there is no operation expression
+        return -1.0; //returns -1
     }
     int count = 0;
-    std::vector<op>::iterator p_count = postfix_stack.begin();
-    while (postfix_stack.size() > 1)
+    std::vector<op>::iterator p_count = postfix_vector.begin(); // iterator of vector(used like pointer)
+    //this while loop performs the calculation sequentially
+    //until there remains only one value(=result, return value)
+    while (postfix_vector.size() > 1) 
     {
-#if 0
-        if (postfix_stack[count].getIsOperand() == false) { // if this op is operator
-            postfix_stack[count - 2].setValue( SimpleCalc( postfix_stack[count-2].getValue(), 
-                postfix_stack[count - 1].getValue(), postfix_stack[count].getValue() ) );
-            postfix_stack.erase(count);
-            postfix_stack.erase(count - 1);
-            count--;
-        }
-#endif
         if (p_count->getIsOperand() == false) { // if this op is operator
             (p_count-2)->setValue( SimpleCalc( (p_count - 2)->getValue(),
                 (p_count - 1)->getValue(), (p_count)->getValue() ) );
             p_count -= 2;
-            postfix_stack.erase(p_count+1);
-            p_count = postfix_stack.erase(p_count+1);
+            postfix_vector.erase(p_count+1);
+            p_count = postfix_vector.erase(p_count+1);
         }
         else {
-            p_count++;
+            p_count++; // moves to next op class if previous one was a operand
         }
-
     }
 
-    float result = postfix_stack.begin()->getValue(); //return value
+    float result = postfix_vector.begin()->getValue(); // value to be returned
     return round((result * 100)) * float(0.01);
 }
 
@@ -112,42 +95,34 @@ int main(int argc, char** argv)
         paren_left = 0, paren_right, multi, division, addition, subtract
     };
     int OP_precedence[6] = {0, 0, 2, 2, 1, 1};
-    //op_precedence paren_left('(', 0);
-    //op_precedence paren_right(')', 0);
-    //op_precedence multi('*', 1);
-    //op_precedence division('/', 1);
-    //op_precedence addition('+', 2);
-    //op_precedence subtract('-', 2);
-    //stores the operand data and operator data with stack
-    std::stack<char> Operator_stk;
-    //std::stack<int> postfix_stk;
-    std::vector<op> postfix_stk;
+    std::stack<char> Operator_stk; //stores operator data with stack
+    std::vector<op> postfix_vtr;   //stores postfix notation converted from input file through lower codes
     //file read
     FILE* fp_input = fopen(argv[1], "r");
     if (fp_input == nullptr) {
         exit(EXIT_FAILURE);
     }
 
-    //std::string postfix_string = "";
     char scan_char = '\0';
     int scan_int = 0;
     
     while (1)
     {
-        fscanf(fp_input, "%c", &scan_char);
-        if (feof(fp_input)) break; // end of file (file read failed)
+        fscanf(fp_input, "%c", &scan_char); // reads repetitively and receive at scan_char
+        if ( feof(fp_input) ) break; // end of file (file read failed)
+
         switch (scan_char)
         {
         case '(':
-            Operator_stk.push(scan_char);
+            Operator_stk.push(scan_char); // '(' should be pushed without any examination
             break;
         case ')':
-            while (Operator_stk.top() != '(')
+            while (Operator_stk.top() != '(') // pushes every operator to postfix_vtr
             {
-                postfix_stk.push_back(op(false, Operator_stk.top()));
+                postfix_vtr.push_back(op(false, Operator_stk.top()));
                 Operator_stk.pop();
             }
-            Operator_stk.pop();
+            Operator_stk.pop(); // delete '(' for convenience
             break;
         case '*':
         case '/':
@@ -156,34 +131,30 @@ int main(int argc, char** argv)
             if (Operator_stk.size() != 0) {
                 //if the incoming operator has lower or same precedence as the latest operator
                 if (OP_precedence[CharToIndex(scan_char)] <= OP_precedence[CharToIndex(Operator_stk.top())]) {
-                    postfix_stk.push_back(op(false, Operator_stk.top()));
+                    postfix_vtr.push_back(op(false, Operator_stk.top())); // push the previous operator to postfix first
                     Operator_stk.pop();
                 }
             }
-            Operator_stk.push(scan_char);
+            Operator_stk.push(scan_char); // push the operator to Operator_stk
             break;
-        default:
+        default: // this is performed when the input is integer
             fseek(fp_input, -1, SEEK_CUR);
             fscanf(fp_input, "%d", &scan_int);
-            postfix_stk.push_back(op(true, scan_int));
-            //postfix_string.append(std::to_string(scan_int));
+            postfix_vtr.push_back(op(true, scan_int)); // push directly to postfix
         }
     }
-    //move all the remaining operators in operator_stk
+    //move all the remaining operators in operator_stk to postfix
     while (Operator_stk.size() != 0)
     {
-        postfix_stk.push_back(op(false, Operator_stk.top()));
+        postfix_vtr.push_back(op(false, Operator_stk.top()));
         Operator_stk.pop();
     }
     fclose(fp_input);
     fp_input = nullptr;
-#if 1
 
-#endif
-#if 1
-    float result = Calc(postfix_stk);
-    printf("The output is : %.2f\n", result);
-#endif
+    //calculates the postfix notation
+    float result = Calc(postfix_vtr);
+
     //writing the result to output text file(argv[2])
     FILE* fp_output = fopen(argv[2], "w");
     if (fp_output == nullptr) {
@@ -192,14 +163,6 @@ int main(int argc, char** argv)
     fprintf(fp_output, "%.2f", result);
     fclose(fp_output);
     fp_output = nullptr;
-#if 0
-    int example = 0;
-    char ex2 = '\0';
-    fscanf(fp_input, "%d", &example);
-    fscanf(fp_input, "%c", &ex2);
-    fscanf(fp_input, "%d", &example);
-#endif
-
 
     return 0;
 }
